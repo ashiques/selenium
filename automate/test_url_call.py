29ae705801ce355a1267b2b0d54137d26daa3ddc
ashiques12 <ashiques12@gmail.com>
2021-12-09 23:37:56 +1100

    adding changes for new driver and more detailed code usage

--- automate/test_url_call.py
+++ automate/test_url_call.py
@@ -2,26 +2,26 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-driver = webdriver.Chrome("/Users/ashique_s/projects/selenium/chromedriver")
+driver = webdriver.Chrome("/Users/ashique/projects/python/selenium/chromedriver")
 driver.get(
     "https://bmvs.onlineappointmentscheduling.net.au/oasis/ModifyAppointment2.aspx"
 )
 
 elem = driver.find_element_by_id("ContentPlaceHolder1_txtHAPID")
 elem.clear()
-elem.send_keys("25717187")
+elem.send_keys("26696764")
 
 first_name = driver.find_element_by_id("ContentPlaceHolder1_txtFirstName")
 first_name.clear()
-first_name.send_keys("Ashique")
+first_name.send_keys("Sajani")
 
 last_name = driver.find_element_by_id("ContentPlaceHolder1_txtSurname")
 last_name.clear()
-last_name.send_keys("Sirajudeen")
+last_name.send_keys("Narayanankutty")
 
 dob = driver.find_element_by_id("ContentPlaceHolder1_txtDOB")
 dob.clear()
-dob.send_keys("08/08/1992")
+dob.send_keys("3/03/1993")
 
 elem.send_keys(Keys.RETURN)
 
@@ -43,17 +43,21 @@ while flag:
     time.sleep(7)
 
     date = data_selector.get_property("value")
+    print(date)
     month = date.split("/")[1]
     day = date.split("/")[0]
+    year = date.split("/")[2]
 
-    if int(month) == 1 and int(day) < 31:
+    if int(month) < 4 and int(day) < 31 and int(year) <=2022:
         flag = False
     else:
         elem = driver.find_element_by_xpath(
-            '//*[@id="form1"]/div[3]/div[2]/div/div[2]/div[6]/button[2]'
+            '//*[@id="form1"]/div[3]/div[2]/div/div[2]/div[7]/button[2]'
         )
+
+        elem2 = driver.find_element_by_xpath('//*[@id="ContentPlaceHolder1_SelectTime1_divSearchResults"]/div[1]/h2')
         # identify element
-        driver.execute_script("window.scrollTo(0,100);")
+        driver.execute_script("arguments[0].scrollIntoView(true);",elem2)
 
         time.sleep(4)
         elem.click()
